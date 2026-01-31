e78bf992144228cfbef1ad63bfeef2eefd2c5794
mme324 <mme324@nyu.edu>
2026-01-31 15:45:49 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -19,7 +19,7 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = int(4)
+        answer = int(7)
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = int(3)
     else: 
@@ -48,6 +48,7 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 
 
 
