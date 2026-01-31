6672a1f1459b40c078f4d75f062647a6ff15a031
mme324 <mme324@nyu.edu>
2026-01-31 15:16:42 -0500

    gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,19 +1,33 @@
 ### welcome_assignment_answers
 ### Input - All nine questions given in the assignment.
-### Output - The right answer for the specific question.
+### Output - My answers for the specific questions.
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
+    #Students do not have to follow the skeleton for this assignment. But I will.
     #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "5a9f0ad0235dc0c2155b13f01f2ddfbeef0a8a4525a4c7f75c95df8f20ce7781"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 4
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
     else: 
-        ### you should understand why this else case should be included
+        ### you should understand why this else case should be included, breaks us out of the loop!
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?/Error"
     return(answer)
 # Complete all the questions.
 
@@ -23,6 +37,8 @@ if __name__ == "__main__":
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
+    debug_question2 = "Is it possible to decrypt a message without a key? - Yes/No"
+    print(welcome_assignment_answers(debug_question2))
 #Questions:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
 #"Are encoding and encryption the same? - Yes/No":
@@ -33,3 +49,4 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
